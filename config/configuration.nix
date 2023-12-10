7f42557779fb457af50b9a02d394ddebcbb64815
jdirkse <jdirkse@mailme.not>
2023-12-11 00:17:45 +0100

    fix: ipv4.addresses list

--- config/configuration.nix
+++ config/configuration.nix
@@ -18,10 +18,12 @@
     hostName = "homeserver";
     domain = "lan.mejora.dev";
     useDHCP = false;
-    interfaces.enp2s0.ipv4.addresses = {
-      address = "192.168.178.2";
-      prefixLength = 24;
-    };
+    interfaces.enp2s0.ipv4.addresses = [
+      {
+        address = "192.168.178.2";
+        prefixLength = 24;
+      }
+    ];
     defaultGateway = "192.168.178.1";
     nameservers = "192.168.178.1";
     firewall = {
