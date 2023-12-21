074a2c40477aff3b15e7f11842a3ad123a093955
jdirkse <jdirkse@mailme.not>
2023-12-21 18:11:54 +0100

    fix

--- config/users.nix
+++ config/users.nix
@@ -6,7 +6,7 @@
         isNormalUser = true;
         extraGroups = [ "wheel" "ssh-users" "docker" ]; # Enable ‘sudo’ for the user.
         openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILk9J5Labk84GXRWOUBETbIhEw8kKq/jR5aISL52/HBH joren@nixos"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILk9J5Labk84GXRWOUBETbIhEw8kKq/jR5aISL52/HBH home@nixos"
         ];
         createHome = true;
       };
