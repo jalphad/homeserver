cb0c100e9754e9d22905be44cc12ddb654f5bc52
jdirkse <jdirkse@mailme.not>
2023-12-21 23:52:50 +0100

    fix adguardhome settings

--- config/services.nix
+++ config/services.nix
@@ -85,9 +85,7 @@ in
       settings = {
         dns = {
           upstream_dns = [ "1.1.1.1" ];
-          filtering = {
-            rewrites = dnsRecords;
-          };
+          rewrites = dnsRecords;
         };
       };
     };
