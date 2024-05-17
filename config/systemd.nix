6b7d91c940747e284a1f2a235daefedf297eb892
jdirkse <jdirkse@mailme.not>
2024-05-17 21:03:27 +0200

    freeradius compose config in nix

--- config/systemd.nix
+++ config/systemd.nix
@@ -1,3 +1,8 @@
+{pkgs,settings,...}:
+
+let 
+  freeradius = import ./compose/freeradius.nix {pkgs=pkgs;settings=settings;};
+in
 {
   systemd = {
     services = {
@@ -20,6 +25,28 @@
           ];
           ExecStart = "/etc/nixos/resources/scripts/ssocert.sh";
           ExecStop = "/run/current-system/sw/bin/docker compose -f /etc/nixos/resources/docker-compose/lego.yml down -v";
+        };
+        wantedBy = [ "multi-user.target" ];
+      };
+      freeradius = {
+        unitConfig = {
+          Description = "Run freeradius in Docker container";
+          After = "docker.service network-online.target";
+          Requires = "network-online.target";
+        };
+        serviceConfig = {
+          Environment="PATH=/run/current-system/sw/bin";
+          RemainAfterExit = "true";
+          Type = "oneshot";
+          TimeoutStartSec = "0";
+          WorkingDirectory = "/home/freeradius";
+          ExecStartPre = [
+            "-/run/current-system/sw/bin/docker compose -f ${freeradius.compose} down -v"
+            "-/run/current-system/sw/bin/docker compose -f ${freeradius.compose} rm -v"
+            "-/run/current-system/sw/bin/docker compose -f ${freeradius.compose} pull"
+          ];
+          ExecStart = "/run/current-system/sw/bin/docker compose -f ${freeradius.compose} up -v";
+          ExecStop = "/run/current-system/sw/bin/docker compose -f ${freeradius.compose} down -v";
         };
         wantedBy = [ "multi-user.target" ];
       };
