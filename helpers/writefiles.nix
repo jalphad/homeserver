a6f19e0668b0a97c2cf1985c5b402cafe2be51a2
jdirkse <jdirkse@mailme.not>
2024-05-20 21:39:25 +0200

    create  dir

--- helpers/writefiles.nix
+++ helpers/writefiles.nix
@@ -4,11 +4,12 @@ with pkgs;
 let
   writeFiles = name: fileInfoList:
     let
+      prepare = ''mkdir -p $out'';
       f = builtins.foldl' (acc: elem:
         (acc + ''
           echo '${elem.text}' > $out/${elem.name}
         ''));
-      script = writeShellScript "builder.sh" (f '''' fileInfoList);
+      script = writeShellScript "builder.sh" (f prepare fileInfoList);
     in
     stdenv.mkDerivation rec {
       inherit name;
